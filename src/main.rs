e95af9c31ff658ec1a2d610b8964eac92d9d3920
exodecode <nicholas.lee.work@gmail.com>
2021-11-17 11:24:40 -0700

    white noise generation

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,26 @@
 use image::{ImageBuffer, RgbImage};
 use rand::prelude::*;
 use rand_pcg::Pcg64;
-use rand_seeder::{Seeder, SipHasher};
+use rand_seeder::Seeder;
 
-const WIDTH: usize = 128;
-const HEIGHT: usize = 128;
-const NAME: &str = "image.png";
+const WIDTH: usize = 4;
+const HEIGHT: usize = 4;
+const NAME: &str = "image1.png";
 const SEED: &str = "asdf";
 
 pub fn main() {
-    let rng: Pcg64 = Seeder::from("stripy zebra").make_rng();
     //let grid = grid(&checkerboard);
-    let grid = grid(&white_noise);
+    let grid = white_noise();
     grid_to_image(grid, NAME);
 }
 
-fn checkerboard(x: usize, y: usize) -> u8 {
-    if x % 2 == y % 2 {
-        1
-    } else {
-        0
-    }
-}
-
-fn r() -> u8{
-    if &rng.gen_range(0..2) % 2 == 0 {0} else {1}
-}
-
-fn white_noise(x: usize, y: usize) -> u8 {
-    //rand::thread_rng().gen_range(0..2)
-}
-
-fn grid(f: &dyn Fn(usize, usize) -> u8) -> [[u8; HEIGHT]; WIDTH] {
+fn white_noise() -> [[u8; HEIGHT]; WIDTH] {
     let mut grid = [[0 as u8; HEIGHT]; WIDTH];
+    let mut rng: Pcg64 = Seeder::from(SEED).make_rng();
 
     for y in 0..HEIGHT {
         for x in 0..WIDTH {
-            grid[x][y] = f(x, y);
+            grid[x][y] = rng.gen_range(0..2);
         }
     }
 
@@ -63,3 +47,24 @@ fn grid_to_image(array: [[u8; HEIGHT]; WIDTH], name: &str) {
 
     image.save(name).unwrap();
 }
+
+//fn grid(f: &dyn Fn(usize, usize) -> u8) -> [[u8; HEIGHT]; WIDTH] {
+    //let mut grid = [[0 as u8; HEIGHT]; WIDTH];
+
+    //for y in 0..HEIGHT {
+        //for x in 0..WIDTH {
+            //grid[x][y] = f(x, y);
+        //}
+    //}
+
+    //grid
+//}
+
+//fn checkerboard(x: usize, y: usize) -> u8 {
+    //if x % 2 == y % 2 {
+        //1
+    //} else {
+        //0
+    //}
+//}
+
